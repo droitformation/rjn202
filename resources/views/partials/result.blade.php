131367b1e8aaef67b9a1e3829a36294dd913ff05
Cindy Leschaud <cindy.leschaud@gmail.com>
2020-04-29 08:59:58 +0200

    fix title terms resutls search

--- resources/views/partials/result.blade.php
+++ resources/views/partials/result.blade.php
@@ -33,7 +33,7 @@
     @else
         <div class="arrets-list-item row">
             <div class="col-md-8">
-                <h4><a href="{{ url($url.'/'.$path) }}">{{ $arret->designation or $arret->titre }}</a></h4>
+                <h4><a href="{{ url($url.'/'.$path) }}">{{ $arret->designation ?? $arret->titre }}</a></h4>
             </div>
             <div class="col-md-3">page {{ $arret->page }}, Volume {{ $rjn->find($arret->volume_id)->publication_at->year }}</div>
             <div class="col-md-1"><a class="btn btn-sm btn-default" href="{{ url($url.'/'.$path) }}">Voir</a></div>
